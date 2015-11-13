089c3e488b77f1cb01b2c8a72259526c84920fa1
Max Hollmann <mail@maxhollmann.de>
2015-11-13 17:47:47 +0100

    fix logging

--- app/jobs/check_for_slots_job.rb
+++ app/jobs/check_for_slots_job.rb
@@ -5,24 +5,24 @@ class Bot
 
   include Capybara::DSL
 
-  def initialize(concern_id = 326541)
+  attr_reader :logger
+
+  def initialize(concern_id = 326541, logger: Rails.logger)
     @concern_id = concern_id
+    @logger = logger
   end
 
   def wait(min = 1, max = 5)
     sleep min + rand * (max - min)
   end
 
-  def day_selection_path
-    "https://service.berlin.de/terminvereinbarung/termin/tag.php?termin=1&dienstleister[]=122210&dienstleister[]=122217&dienstleister[]=122219&dienstleister[]=122227&dienstleister[]=122231&dienstleister[]=122238&dienstleister[]=122243&dienstleister[]=122252&dienstleister[]=122260&dienstleister[]=122262&dienstleister[]=122254&dienstleister[]=122271&dienstleister[]=122273&dienstleister[]=122277&dienstleister[]=122280&dienstleister[]=122282&dienstleister[]=122284&dienstleister[]=122291&dienstleister[]=122285&dienstleister[]=122286&dienstleister[]=122296&dienstleister[]=150230&dienstleister[]=122301&dienstleister[]=122297&dienstleister[]=122294&dienstleister[]=122312&dienstleister[]=122314&dienstleister[]=122304&dienstleister[]=122311&dienstleister[]=122309&dienstleister[]=317869&dienstleister[]=324433&dienstleister[]=325341&dienstleister[]=324434&dienstleister[]=324435&dienstleister[]=122281&dienstleister[]=324414&dienstleister[]=122283&dienstleister[]=122279&dienstleister[]=122276&dienstleister[]=122274&dienstleister[]=122267&dienstleister[]=122246&dienstleister[]=122251&dienstleister[]=122257&dienstleister[]=122208&dienstleister[]=122226&anliegen[]=120686&herkunft=%2Fterminvereinbarung%2F"
+  def goto_day_selection
+    logger.info "Going to day selection"
+    visit "https://service.berlin.de/terminvereinbarung/termin/tag.php?termin=1&dienstleister[]=122210&dienstleister[]=122217&dienstleister[]=122219&dienstleister[]=122227&dienstleister[]=122231&dienstleister[]=122238&dienstleister[]=122243&dienstleister[]=122252&dienstleister[]=122260&dienstleister[]=122262&dienstleister[]=122254&dienstleister[]=122271&dienstleister[]=122273&dienstleister[]=122277&dienstleister[]=122280&dienstleister[]=122282&dienstleister[]=122284&dienstleister[]=122291&dienstleister[]=122285&dienstleister[]=122286&dienstleister[]=122296&dienstleister[]=150230&dienstleister[]=122301&dienstleister[]=122297&dienstleister[]=122294&dienstleister[]=122312&dienstleister[]=122314&dienstleister[]=122304&dienstleister[]=122311&dienstleister[]=122309&dienstleister[]=317869&dienstleister[]=324433&dienstleister[]=325341&dienstleister[]=324434&dienstleister[]=324435&dienstleister[]=122281&dienstleister[]=324414&dienstleister[]=122283&dienstleister[]=122279&dienstleister[]=122276&dienstleister[]=122274&dienstleister[]=122267&dienstleister[]=122246&dienstleister[]=122251&dienstleister[]=122257&dienstleister[]=122208&dienstleister[]=122226&anliegen[]=120686&herkunft=%2Fterminvereinbarung%2F"
     #"https://service.berlin.de/terminvereinbarung/termin/tag.php?termin=1&dienstleister=326541&anliegen[]=121921&herkunft=1"
   end
 
   def goto_first_free_date
-    logger.info "Visiting #{day_selection_path}"
-
-    visit day_selection_path
-
     months_ahead = 0
 
     begin
@@ -106,8 +106,9 @@ class CheckForSlotsJob < ActiveJob::Base
   queue_as :default
 
   def perform
-    bot = Bot.new
+    bot = Bot.new(logger: logger)
 
+    bot.goto_day_selection
     bot.goto_first_free_date
     bot.goto_first_free_time
 
